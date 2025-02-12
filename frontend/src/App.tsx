67e45859110ec61b62e067e41cb1345b8a62ad4f
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-12 18:22:37 +0000

    Set up routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css'
+import DummyNav from './DummyNav';
+import Home from './dummy-pages/Home';
+import Page1 from './dummy-pages/Page1';
+import Page2 from './dummy-pages/Page2';
 
 function App() {
 
   return (
     <>
-      <h1 className="bg-blue-500 text-3xl font-bold underline">Welcome to LogiQuest</h1>
+    <BrowserRouter>
+    <DummyNav/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/page1' element={<Page1/>} />
+      <Route path='/page2' element={<Page2/>} />
+      {/* <h1 className="bg-blue-500 text-3xl font-bold underline">Welcome to LogiQuest</h1> */}
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
