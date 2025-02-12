d447520e1b9c64fd92325ffda88d848301d21697
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-12 18:34:50 +0000

    added the heading back

--- frontend/src/dummy-pages/Home.tsx
+++ frontend/src/dummy-pages/Home.tsx
@@ -4,7 +4,7 @@
 function Home() {
     return (
         <div>
-            home
+            <h1 className="bg-blue-500 text-3xl font-bold underline">Welcome to LogiQuest</h1>
         </div>
     )
 }
