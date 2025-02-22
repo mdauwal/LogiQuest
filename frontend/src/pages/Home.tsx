146f2d9e0ad227d3ead1fb5850ce97d129bd52c8
Elite-tch <elite6461@gmail.com>
2025-02-22 10:11:54 +0100

    i create a why should you play section, imported it inside home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import FaqsSection from "../components/FaqsSection";
 import Footer from "../components/Footer";
-
+import WhyShouldYouPlaySection from "../components/WhyShouldYouPlaySection";
 const Home = () => {
   return (
     <>
       {/* paste your page component below here */}
+        <WhyShouldYouPlaySection/>
         <FaqsSection/>
+
         <Footer />
       {/* paste your page component above here */}
     </>
