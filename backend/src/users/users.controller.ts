d88a7b89b5c80e04ce582f816b129188b03723b1
nafiuishaaq <nafiuig@gmail.com>
2025-02-24 10:32:28 +0100

    fixed the error from users module

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common"
-import type { UsersService } from "./users.service"
-import type { CreateUserDto } from "./dto/create-user.dto"
-import type { UpdateUserDto } from "./dto/update-user.dto"
+import { UsersService } from "./users.service"
+import { CreateUserDto } from "./dto/create-user.dto"
+import  { UpdateUserDto } from "./dto/update-user.dto"
 import { ApiTags, ApiOperation, ApiResponse, ApiParam } from "@nestjs/swagger"
 
 @ApiTags("users")
