b669c0529e1f622aa6584df3dfd0b0218d13fdce
cyphoriatech <cyphoriatech@gmail.com>
2025-03-20 19:30:14 +0100

    Re-commit

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 @Module({
   imports: [
