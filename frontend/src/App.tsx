530788367a8a1e20c4615c759294996ae7768944
Fatima Aliyu <aliyufatima916@gmail.com>
2025-03-22 10:04:55 +0100

    redesigned hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home'; // Home page component
+import LandingPage from './components/HeroSection ';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* <Route path='/' element={<LandingPage />} /> */}
         <Route path='/' element={<Home />} /> {/* Default home page */}
       </Routes>
     </BrowserRouter>
