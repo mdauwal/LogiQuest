3a334008cbced83acef9376f8ad193340fb989b4
bigchills <yemi7164@gmail.com>
2025-03-22 19:12:42 +0100

    chore:clean up home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,17 +3,17 @@ import Footer from "../components/Footer";
 import AboutUsSection from "../components/AboutUsSection";
 import WhyShouldYouPlaySection from "../components/WhyShouldYouPlaySection";
 import Navbar from "../components/Navbar";
+import GamePlay from "../components/Gameplay/GamePlay";
 const Home = () => {
   return (
     <>
       {/* paste your page component below here */}
-       
 
-        <Navbar />
-         <WhyShouldYouPlaySection/>
-        <FaqsSection/>
-        <AboutUsSection />
-        <Footer />
+      <Navbar />
+      <WhyShouldYouPlaySection />
+      <FaqsSection />
+      <AboutUsSection />
+      <Footer />
       {/* paste your page component above here */}
     </>
   );
