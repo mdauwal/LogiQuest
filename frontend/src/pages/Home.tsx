10c83257fb6698da2cd7859e6342b7e5731976dd
MarcusDavidG <davidmarcus827@gmail.com>
2025-03-25 13:49:28 +0100

    Redesigned 'How to Play' section on landing page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import FaqsSection from "../components/FaqsSection";
+import HowToPlay from "../components/HowToPlay";
 import Footer from "../components/Footer";
 import AboutUsSection from "../components/AboutUsSection";
 import WhyShouldYouPlaySection from "../components/WhyShouldYouPlaySection";
@@ -12,7 +13,8 @@ const Home = () => {
 
         <Navbar />
         <HeroSection />
-         <WhyShouldYouPlaySection />
+         <HowToPlay />
+        <WhyShouldYouPlaySection />
         <FaqsSection/>
         <AboutUsSection />
         <Footer />
