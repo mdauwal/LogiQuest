61da83f4181883be44a2411e34feb98298dcb125
Ahmed Mahmud <ahmedmahmud1994@hotmail.com>
2025-03-26 00:13:08 +0000

    updated login page theme

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,25 +1,78 @@
 export default function Login() {
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-[#141516]">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                    <img
-                        alt="Your Company"
-                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto"
-                    />
-                    <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                        Sign in to your account
+                    <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-[#0A746D]">
+                        Sign in with
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-                    <div className="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
+                    <div className="w-4/5 mx-auto flex flex-col gap-4">
+                        <a
+                            href="#"
+                            className="flex w-full items-center justify-center gap-3 rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-[#0A746D] ring-inset hover:bg-gray-800 focus-visible:ring-transparent"
+                        >
+                            <span className="text-sm/6 font-semibold text-[#0A746D]">Google</span>
+                            <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
+                                <path
+                                    d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
+                                    fill="#EA4335"
+                                />
+                                <path
+                                    d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
+                                    fill="#4285F4"
+                                />
+                                <path
+                                    d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
+                                    fill="#FBBC05"
+                                />
+                                <path
+                                    d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
+                                    fill="#34A853"
+                                />
+                            </svg>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="flex w-full items-center justify-center gap-3 rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-[#0A746D] ring-inset hover:bg-gray-800 focus-visible:ring-transparent"
+                        >
+                            <span className="text-sm/6 font-semibold text-[#0A746D]">Apple</span>
+                            <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
+                                <path
+                                    d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"
+                                    fill="#ffffff"
+                                />
+                            </svg>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="flex w-full items-center justify-center gap-3 rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-[#0A746D] ring-inset hover:bg-gray-800 focus-visible:ring-transparent"
+                        >
+                            <span className="text-sm/6 font-semibold text-[#0A746D]">Microsoft</span>
+                            <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
+                                <path d="M11.5 3H3v9.5h8.5V3z" fill="#f25022" />
+                                <path d="M11.5 12H3v9.5h8.5V12z" fill="#7fba00" />
+                                <path d="M21 3h-8.5v9.5H21V3z" fill="#00a4ef" />
+                                <path d="M21 12h-8.5v9.5H21V12z" fill="#ffb900" />
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div className="relative mt-10 w-4/5 mx-auto">
+                        <div aria-hidden="true" className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-[#033330]" />
+                        </div>
+                        <div className="relative flex justify-center text-sm/6 font-medium">
+                            <span className="bg-[#141516] px-6 text-[#033330]">Or</span>
+                        </div>
+                    </div>
+
+                    <div className="bg-tranparent px-6 py-6 shadow-sm sm:rounded-lg sm:px-12">
                         <form action="#" method="POST" className="space-y-6">
-                            <div>
-                                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                                    Email address
-                                </label>
                                 <div className="mt-2">
                                     <input
                                         id="email"
@@ -27,15 +80,12 @@ export default function Login() {
                                         type="email"
                                         required
                                         autoComplete="email"
-                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                        placeholder="Email"
+                                        className="border block w-full rounded-md bg-transparent px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                     />
                                 </div>
-                            </div>
 
                             <div>
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                                    Password
-                                </label>
                                 <div className="mt-2">
                                     <input
                                         id="password"
@@ -43,7 +93,8 @@ export default function Login() {
                                         type="password"
                                         required
                                         autoComplete="current-password"
-                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                        placeholder="****"
+                                        className="border block w-full rounded-md bg-transparent px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                     />
                                 </div>
                             </div>
@@ -80,13 +131,13 @@ export default function Login() {
                                             </svg>
                                         </div>
                                     </div>
-                                    <label htmlFor="remember-me" className="block text-sm/6 text-gray-900">
+                                    <label htmlFor="remember-me" className="block text-sm/6 text-gray-300">
                                         Remember me
                                     </label>
                                 </div>
 
                                 <div className="text-sm/6">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    <a href="#" className="font-semibold text-[#0A746D] hover:text-indigo-500">
                                         Forgot password?
                                     </a>
                                 </div>
@@ -95,70 +146,18 @@ export default function Login() {
                             <div>
                                 <button
                                     type="submit"
-                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="flex w-full justify-center rounded-md bg-[#033330] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Sign in
                                 </button>
                             </div>
                         </form>
-
-                        <div>
-                            <div className="relative mt-10">
-                                <div aria-hidden="true" className="absolute inset-0 flex items-center">
-                                    <div className="w-full border-t border-gray-200" />
-                                </div>
-                                <div className="relative flex justify-center text-sm/6 font-medium">
-                                    <span className="bg-white px-6 text-gray-900">Or continue with</span>
-                                </div>
-                            </div>
-
-                            <div className="mt-6 grid grid-cols-2 gap-4">
-                                <a
-                                    href="#"
-                                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
-                                >
-                                    <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
-                                        <path
-                                            d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
-                                            fill="#EA4335"
-                                        />
-                                        <path
-                                            d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
-                                            fill="#4285F4"
-                                        />
-                                        <path
-                                            d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
-                                            fill="#FBBC05"
-                                        />
-                                        <path
-                                            d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
-                                            fill="#34A853"
-                                        />
-                                    </svg>
-                                    <span className="text-sm/6 font-semibold">Google</span>
-                                </a>
-
-                                <a
-                                    href="#"
-                                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
-                                >
-                                    <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" className="size-5 fill-[#24292F]">
-                                        <path
-                                            d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                                            clipRule="evenodd"
-                                            fillRule="evenodd"
-                                        />
-                                    </svg>
-                                    <span className="text-sm/6 font-semibold">GitHub</span>
-                                </a>
-                            </div>
-                        </div>
                     </div>
 
-                    <p className="mt-10 text-center text-sm/6 text-gray-500">
-                        Not a member?{' '}
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            Start a 14 day free trial
+                    <p className="text-center text-sm/6 text-gray-500">
+                        Don't have an account?{' '}
+                        <a href="#" className="font-semibold text-[#0A746D] hover:text-indigo-500">
+                            Create One
                         </a>
                     </p>
                 </div>
