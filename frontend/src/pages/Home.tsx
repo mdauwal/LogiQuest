a818d79ebb2d302ce0245ca96f88213abeff43f2
kamaldeen <kamalaliyu503@gmail.com>
2025-03-27 14:48:35 +0100

    Account Settings page was created

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import AboutUsSection from "../components/AboutUsSection";
 import WhyShouldYouPlaySection from "../components/WhyShouldYouPlaySection";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection ";
+import AccountSettings from "../components/AccountSettings";
+
 const Home = () => {
   return (
     <>
@@ -15,6 +17,7 @@ const Home = () => {
          <WhyShouldYouPlaySection />
         <FaqsSection/>
         <AboutUsSection />
+        <AccountSettings />
         <Footer />
       {/* paste your page component above here */}
     </>
